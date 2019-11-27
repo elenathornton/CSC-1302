0c3a7b7bf65e4848950b02b5d3e7c24697325a70
Elena Thornton <30423073+elenathornton@users.noreply.github.com>
2019-11-26 22:46:45 -0500

    Create Fahrenheit.java

---
+++ LAB/csc1302lab13/Fahrenheit.java
@@ -0,0 +1,17 @@
+package csc1302lab13;
+import javax.swing.JFrame;
+
+
+public class Fahrenheit {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Fahrenheit");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		FahrenheitPanel panel = new FahrenheitPanel();
+		frame.getContentPane().add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+	}
+}
